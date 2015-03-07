9bfc59b0b27a6c11ce15edbdf87ab38d80759460
helenanderson <helenanderson@stanford.edu>
2015-03-06 16:42:06 -0800

    js cleaned up

---
+++ js/driving.js
@@ -0,0 +1,34 @@
+function StartStop(textid, circleid) {
+	this.text = document.getElementById(textid);
+	this.circle = document.getElementById(circleid);
+
+	var button = this;
+	$(this.text, this.circle).click(function() {
+		if ($(button.text).attr("class") == "start") {
+			button.start();
+		} else {
+			button.stop();
+		}
+	})
+};
+
+StartStop.prototype.drawCircle = function(color) {
+	var context = this.circle.getContext("2d");
+	context.clearRect(0,0,context.canvas.width,context.canvas.height);
+	context.arc(350, 900, 160, 0, Math.PI * 2, false);
+    context.lineWidth = 25;
+	context.strokeStyle = color;
+	context.stroke();
+}
+
+StartStop.prototype.start = function() {
+	$(this.text).html("STOP");
+	$(this.text).attr("class", "stop");
+	this.drawCircle("orangered");
+}
+
+StartStop.prototype.stop = function() {
+	$(this.text).html("START");
+	$(this.text).attr("class", "start");
+	this.drawCircle("deepskyblue");
+}
